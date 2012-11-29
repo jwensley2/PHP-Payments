9f3180a30c5caa71f7b610439b0a995acb47bec9
Joseph Wensley <jwensley2@gmail.com>
2012-11-29 15:12:11 -0500

    remove leading slash so internal links work when looking at docs on the filesystem or in a sub folder

--- documentation/dox/template/header.php
+++ documentation/dox/template/header.php
@@ -6,25 +6,25 @@
 			</a>
 			<ul class="nav pull-right">
 				<li class="<?php echo ($page == 'index.php') ? 'active' : '';?>">
-					<a href="/index.html">Overview</a>
+					<a href="index.html">Overview</a>
 				</li>
 				<li class="<?php echo ($page == 'contributors.php') ? 'active' : '';?>">
-					<a href="/contributors.html">Contributors</a>
+					<a href="contributors.html">Contributors</a>
 				</li>
 				<!--<li class="<?php echo ($page == 'frameworks.php') ? 'active' : '';?>">
-					<a href="/frameworks.html">Frameworks</a>
+					<a href="frameworks.html">Frameworks</a>
 				</li>-->
 				<li class="<?php echo ($page == 'usage.php') ? 'active' : '';?>">
-					<a href="/usage.html">Usage</a>
+					<a href="usage.html">Usage</a>
 				</li>
 				<li class="<?php echo ($page == 'methods.php') ? 'active' : '';?>">
-					<a href="/methods.html">Methods</a>
+					<a href="methods.html">Methods</a>
 				</li>
 				<li class="<?php echo ($page == 'drivers.php') ? 'active' : '';?>">
-					<a href="/drivers.html">Driver Specific Documentation</a>
+					<a href="drivers.html">Driver Specific Documentation</a>
 				</li>
 				<li class="<?php echo ($page == 'add_driver.php') ? 'active' : '';?>">
-					<a href="/add_drivers.html">Adding Drivers</a>
+					<a href="add_drivers.html">Adding Drivers</a>
 				</li>
 			</ul>
 		</div>
